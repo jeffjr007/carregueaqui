cba3143814194623fc1437fe26885737bb3413df
Jeferson Junior <jeffjr007z@gmail.com>
2025-06-07 22:37:13 -0300

    corrigindo erro de JSX sem elemento pai e importando Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Route } from "react-router-dom";
 import Terms from "@/pages/Terms";
 import PrivacyPolicy from "@/pages/PrivacyPolicy";
 import AboutApp from "@/pages/AboutApp";
 
-<Route path="/termos" element={<Terms />} />
-<Route path="/privacidade" element={<PrivacyPolicy />} />
-<Route path="/sobre" element={<AboutApp />} /> 
+<>
+  <Route path="/termos" element={<Terms />} />
+  <Route path="/privacidade" element={<PrivacyPolicy />} />
+  <Route path="/sobre" element={<AboutApp />} />
+</> 
